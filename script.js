f3de2e4f0d701523f76b5f4c14ddf2b7e9b864e6
Ramanathans2 <ramdeepan2@gmail.com>
2025-03-17 16:58:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function openmar19(){
+    window.location = "march-events/march-19-events.html"
+}
+
+
+function openmar22(){
+    window.location = "march-events/march-22-events.html"
+}
+
+function openmar23(){
+    window.location = "march-events/march-23-events.html";
+}
+
+
+function openmar24(){
+    window.location = "march-events/march-24-events.html";
+}
+
+
