6f32de68de4944b04ffa20e62b85b61c89d1347d
Ramanathans2 <ramdeepan2@gmail.com>
2025-03-17 17:01:06 +0530

    march_events/

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-function openmar19(){
-    window.location = "march-events/march-19-events.html"
+
+function open191(){
+    window.location = "https://www.srmacevdp.com/acehack/";
+}
+function open192(){
+    window.location = "https://clusiter2k25.dreamyoursinfotech.com/";
+}
+function open193(){
+    window.location = "https://docs.google.com/forms/d/e/1FAIpQLSdu244t8UnMwcgz683wzO4hVoFCHCueKX2iC7ju4EYpOxXOOQ/viewform";
+}
+function open194(){
+    window.location = "https://www.rewopsrec.in/";
 }
 
 
-function openmar22(){
-    window.location = "march-events/march-22-events.html"
-}
-
-function openmar23(){
-    window.location = "march-events/march-23-events.html";
+function open221(){
+    window.location = "https://dakshaa.ksrct.ac.in/";
 }
 
 
-function openmar24(){
-    window.location = "march-events/march-24-events.html";
+function open231(){
+    window.location = "https://topengineersindia.com/events/";
 }
 
+
+function open241(){
+    window.location = "https://makeathon.mkce.ac.in/";
+}
 
